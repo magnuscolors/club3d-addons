048874bdef17e695b8c817aaa58d86a2dd54e547
kumarinie <kumarisushmanie@gmail.com>
2019-11-25 18:14:11 +0530

    T0381 - stock moves qty_done sum and shows -ve for outgoing qunatity Module: club3d_detailed_sale_report

--- club3d_detailed_sale_report/models/stock.py
+++ club3d_detailed_sale_report/models/stock.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.addons import decimal_precision as dp
+
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
+    
+    @api.depends('quantity_done')
+    def _qty_done_compute(self):
+        for move in self:
+            move.done_qty = move.quantity_done
+            if move.location_dest_id.usage == 'customer':
+                move.done_qty = -move.done_qty
+
+
+    done_qty = fields.Float('Quantity Done456', compute='_qty_done_compute', digits=dp.get_precision('Product Unit of Measure'), store=True)
 
     @api.model
     def create(self, vals):
