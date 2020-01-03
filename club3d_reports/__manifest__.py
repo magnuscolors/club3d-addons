4f974199cf3357861796fffdb86bf74af1deb829
praveen-kumarG <praveenkg@systemsvalley.co.in>
2020-01-03 12:44:11 +0530

    Updated dependency list of Club3D modules to fix attribute errors & Added icons for all the club3d modules

--- club3d_reports/__manifest__.py
+++ club3d_reports/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'stock'],
+    'depends': ['base', 'sale', 'stock', 'stock_picking_invoice_link', 'product_harmonized_system'],
 
     # always loaded
     'data': [
