bda1c1df1ddf6d0d70f144f2b305075fe10499af
kumarinie <kumarisushmanie@gmail.com>
2020-01-07 18:54:08 +0530

    Club3D Report Background image FIX & picking report enhancement Module: club3d_reports

--- club3d_reports/models/res_company.py
+++ club3d_reports/models/res_company.py
@@ -5,8 +5,8 @@ from odoo import models, fields, api
 class Company(models.Model):
     _inherit = 'res.company'
 
-    report_background_image1 = fields.Binary('Background Image for Report Frontpage',
-            help='Set Background Image for Report Frontpage')
+    # report_background_image1 = fields.Binary('Background Image for Report Frontpage',
+    #         help='Set Background Image for Report Frontpage')
 
-    # header_image = fields.Binary('Header Image')
-    # footer_image = fields.Binary('Footer Image')
+    report_header_image = fields.Binary('Default Header Image for Report', help='Set Background Image for Report Header')
+    report_footer_image = fields.Binary('Default Footer Image for Report', help='Set Background Image for Report Footer')
